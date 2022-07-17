410e4ae3f3889aa5c8e676216025d87bf2e1dd4f
Aadaya <aadayagp@gmail.com>
2022-07-17 19:40:45 +0530

    my app first commit

---
+++ website/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+
+    path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('pricing/', views.pricing, name="pricing"),
+    path('appointment/', views.appointment, name="appointment"),
+    path('booknow/', views.booknow, name="booknow"),
+
+]
